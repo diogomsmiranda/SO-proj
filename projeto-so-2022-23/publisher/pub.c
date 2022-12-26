0ff620f080e8f1dd5d15faa7666091ac1ab14bb7
Diogo Miranda <diogomsmiranda@gmail.com>
2022-12-26 16:10:06 +0000

    Revert "2 exercise skeleton"
    This reverts commit 0d3c46e89c48c8dc6653745c7813f55bc882dd8d.

--- projeto-so-2022-23/publisher/pub.c
+++
@@ -1,9 +0,0 @@
-#include "logging.h"
-
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
-}
