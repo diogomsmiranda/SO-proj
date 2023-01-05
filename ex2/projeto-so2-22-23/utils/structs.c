40f9bdfcefb4661220dc66f7360c5456d15aa31a
Diogo Miranda <diogomsmiranda@gmail.com>
2023-01-05 13:49:44 +0000

    done

--- ex2/projeto-so2-22-23/utils/structs.c
+++ ex2/projeto-so2-22-23/utils/structs.c
@@ -8,8 +8,8 @@
 
 void build_message(uint8_t code, char message[MAX_MESSAGE_SIZE], char* buffer) {
     // use memcpy to copy the paramaters to the buffer
-    memcpy(buffer, &code, sizeof(int)); 
-    memcpy(buffer + sizeof(int), message, MAX_MESSAGE_SIZE);
+    memcpy(buffer, &code, sizeof(u_int8_t)); 
+    memcpy(buffer + sizeof(u_int8_t), message, MAX_MESSAGE_SIZE);
 
     return ;
 }
